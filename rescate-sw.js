2e6077410b132ca157e9fee577bc49c9db5842f8
Jose Vasquez <soporte.infotecnology@gmail.com>
2026-06-27 17:03:43 -0500

    Renombrado a index.html para acceso directo

--- rescate-sw.js
+++ rescate-sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'rescapp-v1';
-const FILES = ['rescate.html', 'rescate-manifest.json', 'rescate-icon.svg'];
+const FILES = ['index.html', 'rescate-manifest.json', 'rescate-icon.svg'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
