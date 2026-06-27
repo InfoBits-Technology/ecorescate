b4cf1af7572a2d25317fe0727e019c369a13a70f
Jose Vasquez <soporte.infotecnology@gmail.com>
2026-06-27 16:56:05 -0500

    RescApp: detección de sonidos bajo escombros 🇻🇪

---
+++ rescate-sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'rescapp-v1';
+const FILES = ['rescate.html', 'rescate-manifest.json', 'rescate-icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
